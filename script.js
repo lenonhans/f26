34035bb7827dd219d3655a5c8e2cbeddf21297b6
lenonhans <hans.lenon@studytafensw.edu.au>
2025-02-26 12:03:44 +1100

    new commit

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ for (let i = 1; i <= 100; i++) {
 
 
 let studentGrade;
+studentGrade = 69;
 
 if (studentGrade < 0 || studentGrade > 100) {
-  console.log
+  console.log("Invalid grade");
+} else if (studentGrade >= 90) {
+  console.log("A");
+} else if (studentGrade >= 80) {
+    console.log("B");
+} else if (studentGrade >= 70) {
+    console.log("C");
+} else if (studentGrade >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
